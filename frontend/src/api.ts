83f61ce0510160badb5053d617e1461f0ca45759
Martin Lowe <martinlowe673@gmail.com>
2023-10-30 08:09:20 +0000

    amended api

---
+++ frontend/src/api.ts
@@ -0,0 +1,10 @@
+const searchRecipes = async (searchTerm: string, page:number) =>{
+    const baseURL = new URL ("https://localhost:5005/api/recipes/search");
+baseURL.searchParams.append("searchTerm", searchTerm)
+baseURL.searchParams.append("page", String(page))
+
+const response = await fetch(baseURL)
+if(!response.ok){
+    throw new Error(`HTTP error ! Status ${response.status}`)
+}
+}
