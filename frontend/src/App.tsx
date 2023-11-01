64a02a3cb0e6f4820436dd7314da479d7a16a097
Martin Lowe <martinlowe673@gmail.com>
2023-11-01 08:06:08 +0000

    recipe type

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,30 @@
 
 import { useState } from "react"
 import "./App.css"
+import * as api from "./api"
+import { Recipe } from "./types"
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("burgers")
-  const [recipes,setRecipes] = useState([])
+  const [recipes,setRecipes] = useState<Recipe[]>([])
 
   const handleSearchSubmit = async () => {
     try{
       const recipes = await api.searchRecipes(searchTerm,1)
+      setRecipes(recipes)
     } catch(e) {
       console.log(e);
       
     }
   }
   return(
-    <div>Hello from react</div>
+    <div>{recipes.map((recipe) => (
+      <div>
+        recipe image location: {recipe.image}
+        recipe title:{recipe.title}
+      </div>
+    ))}
+      </div>
   )
 }
 
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,4 @@
-const searchRecipes = async (searchTerm: string, page:number) =>{
+export const searchRecipes = async (searchTerm: string, page:number) =>{
     const baseURL = new URL ("https://localhost:5005/api/recipes/search");
 baseURL.searchParams.append("searchTerm", searchTerm)
 baseURL.searchParams.append("page", String(page))
@@ -7,4 +7,6 @@ const response = await fetch(baseURL)
 if(!response.ok){
     throw new Error(`HTTP error ! Status ${response.status}`)
 }
+
+return response.json()
 }
